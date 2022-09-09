f4b69f63a17ae03a5d2c8b7a8df14a5a2a747a9d
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-09-09 18:25:24 +0900

    [add] feature of reset count

---
+++ app/src/components/CountReset.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let count: number = 0
+</script>
+
+<div>
+  <button on:click={() => (count = 0)}>0</button>
+</div>
+
+<style>
+  div {
+    display: inline-block;
+  }
+</style>
--- app/src/components/CounterCard.svelte
+++ app/src/components/CounterCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import CountUpDown from './CountUpDown.svelte'
+  import CountReset from './CountReset.svelte'
   import type { CounterCardValueType } from '../types'
 
   export let value: CounterCardValueType = { name: '', count: 0 }
@@ -8,8 +9,14 @@
 <div class="card">
   <span>{value.name}</span>
   <span>{value.count}</span>
-  <CountUpDown bind:count={value.count} />
+  <div class="count-controller">
+    <CountUpDown bind:count={value.count} />
+    <CountReset bind:count={value.count} />
+  </div>
 </div>
 
 <style>
+  .count-controller {
+    display: inline-block;
+  }
 </style>
